b517d3254999e6d94ea1dcf9f9fdc74ed6d4ca8f
kevin <pruebamazon475@gmail.com>
2026-09-01 08:43:26 -0600

    inicio del diseño.

--- rpsai/Form1.Designer.cs
+++ rpsai/Form1.Designer.cs
@@ -28,12 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            pnlMenu = new Panel();
+            pnlMenu.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(292, 39);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 15);
+            label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 20F);
+            label2.Location = new Point(106, 28);
+            label2.Name = "label2";
+            label2.Size = new Size(226, 37);
+            label2.TabIndex = 1;
+            label2.Text = "MENU PRINCIPAL";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(126, 113);
+            button1.Name = "button1";
+            button1.Size = new Size(181, 66);
+            button1.TabIndex = 2;
+            button1.Text = "ENTRENAR";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(126, 226);
+            button2.Name = "button2";
+            button2.Size = new Size(181, 66);
+            button2.TabIndex = 3;
+            button2.Text = "JUGAR CON ENTRENAMIENTO";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(126, 333);
+            button3.Name = "button3";
+            button3.Size = new Size(181, 66);
+            button3.TabIndex = 4;
+            button3.Text = "MOSTRAR APRENDIZAJE";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // pnlMenu
+            // 
+            pnlMenu.Anchor = AnchorStyles.None;
+            pnlMenu.BackColor = SystemColors.ControlDark;
+            pnlMenu.Controls.Add(label2);
+            pnlMenu.Controls.Add(button3);
+            pnlMenu.Controls.Add(button1);
+            pnlMenu.Controls.Add(button2);
+            pnlMenu.Location = new Point(195, 12);
+            pnlMenu.Name = "pnlMenu";
+            pnlMenu.Size = new Size(417, 450);
+            pnlMenu.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(838, 524);
+            Controls.Add(pnlMenu);
+            Controls.Add(label1);
+            Name = "Form1";
             Text = "Form1";
+            pnlMenu.ResumeLayout(false);
+            pnlMenu.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Panel pnlMenu;
     }
 }
--- rpsai/Form1.cs
+++ rpsai/Form1.cs
@@ -6,5 +6,7 @@ namespace rpsai
         {
             InitializeComponent();
         }
+
+    
     }
 }
